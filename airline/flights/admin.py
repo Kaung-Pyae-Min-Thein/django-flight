9a7b703925769202b970195653c28479eb663571
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2021-07-25 23:37:07 +0630

    add AdminDesign

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.db import models
 
 from .models import Flight,Airport, Passenger
 # Register your models here.
+
+# admin interface
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('id', 'origin','destination', 'duration')
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal=('flights',)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
